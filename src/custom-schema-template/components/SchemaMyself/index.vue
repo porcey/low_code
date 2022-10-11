2c1b2475360f940e05f1f6c91fefc2b2272ae13a
porcey <827287515@qq.com>
2022-10-11 21:46:36 +0800

    修复了小bug

--- src/custom-schema-template/components/SchemaMyself/index.vue
+++ src/custom-schema-template/components/SchemaMyself/index.vue
@@ -1,37 +1,45 @@
 <template>
-    <div>
-        <div class="skills">
-            <ul>
-                <li>熟悉HTML ，CSS和Javascript ，了解HTML5、CSS3与ES6新特性</li>
-                <li>熟悉JavaScript原型、原型链、闭包、防抖节流等</li>
-                <li>熟悉Vue技术栈及其生态（vue-router,Vuex,Vue-cli等），能独立进行项目开发</li>
-                <li>了解Vue2.X原理（虚拟DOM，Diff算法，响应式原理）以及Vue3的技术更新</li>
-                <li>了解并会使用Element-UI与Scss预编译语言</li>
-                <li>了解HTTP、浏览器缓存与浏览器的渲染机制</li>
-                <li>了解并会使用Git进行版本管理，团队配合意识良好</li>
-            </ul>
-        </div>
-        <div class="project"></div>
+  <div>
+    <div class="skills">
+      <ul>
+        <li>
+          熟悉 HTML ，CSS 和 Javascript ，了解 ES6
+          新特性，JS原型、闭包、防抖节流等
+        </li>
+        <li>
+          熟悉 Vue
+          框架的基本使用，能独立进行项目开发，对Vue有一定的了解，例如虚拟Dom、Di
+          ff算法、响应式原理，以及Vue3的技术更新等
+        </li>
+        <li>熟练掌握Photoshop，能独立设计网页、小程序原型以及海报、logo等</li>
+        <li>了解 HTTP 、浏览器缓存与浏览器的渲染机制等相关知识</li>
+        <li>
+          了解 Element-UI 与 Scss 预编译语言，能够使用 Git
+          进行版本管理，团队配合意识良好
+        </li>
+      </ul>
     </div>
+    <div class="project"></div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "SchemaMyself",
-}
+  name: "SchemaMyself",
+};
 </script>
 
 <style scoped lang="scss">
 .skills {
-    font-size: 18px;
+  font-size: 18px;
 
-    ul {
-        margin: 10px;
-        margin-left: 35px;
+  ul {
+    margin: 10px;
+    margin-left: 35px;
 
-        li {
-            margin-bottom: 20px;
-        }
+    li {
+      margin-bottom: 20px;
     }
+  }
 }
 </style>
